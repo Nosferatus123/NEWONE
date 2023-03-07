d56cca6b54321eacb700f1b3d2e7485be909313a
Nosferatus123 <beqaadamashvili@yahoo.com>
2023-03-07 08:02:42 -0800

    Commit

---
+++ TS1.py
@@ -0,0 +1,2 @@
+name = input("What is your name?").strip().title()
+print("Hello", name , "Nice to meet you")
---
+++ TS3.py
@@ -0,0 +1,10 @@
+name = input("Please enter your name  ").strip().title()
+
+if name[0]=="P" :
+    print("Whats up", name)
+elif name [0]=="D"  :
+    print("Glad to see you")
+elif name[0]=="A" :
+    print("როგორ ხარ",  name)
+else:
+    print("მიმართეთ პრესს ცენტრს", name)
