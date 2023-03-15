0347fdb8af74239e7302bde2d6be625998da253a
Nosferatus123 <beqaadamashvili@yahoo.com>
2023-03-15 07:48:34 -0700

    Commit

---
+++ deep.py
@@ -0,0 +1,9 @@
+name = input("What is the answer to the great question of life, the universe and everything? ")
+if   name == "42":
+        print("Yes")
+elif name  == "forty-two":
+        print("Yes")
+elif name  == "forty two":
+        print("Yes")
+else:
+        print("NO")
