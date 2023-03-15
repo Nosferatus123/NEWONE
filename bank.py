db36bc0d4c00e103a6c939f603d6cfecfe64a1ad
Nosferatus123 <beqaadamashvili@yahoo.com>
2023-03-15 07:49:16 -0700

    Commit

---
+++ bank.py
@@ -0,0 +1,13 @@
+text = input("Greetings: ")
+new_text = text.lower()
+
+if "hello" in new_text:
+    print("$0")
+
+
+elif new_text [0] == "h":
+    print("20$")
+elif new_text == "how you doing?":
+    print("20$")
+else:
+    print("100$")
