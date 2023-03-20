d8adb9368acebd4db6553b3a24d2231b92747c75
Nosferatus123 <beqaadamashvili@yahoo.com>
2023-03-20 02:59:46 -0700

    Commit

---
+++ 12task.py
@@ -0,0 +1,9 @@
+mylist = {
+   "William Shakespeare" : '"To be or not to be"' ,
+   "Lord Tennyson" : '"Tis better to have loved and lost than never to have loved at all"' ,
+   "Aedh" : '"Tread softly because you tread on my dreams"' ,
+   "Rupert Brooke" : '"If I should die, think only this of me"' ,
+   }
+
+proverbs = input("your author: ")
+print(mylist[proverbs])
